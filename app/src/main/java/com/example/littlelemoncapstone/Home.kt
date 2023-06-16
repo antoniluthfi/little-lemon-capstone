0454788e498c9c42dfe7c74ba5fb8a529d576e44
Antoni Luthfi <luthfiantoni027@gmail.com>
2023-06-16 14:30:25 +0800

    create base home screen

---
+++ app/src/main/java/com/example/littlelemoncapstone/Home.kt
@@ -0,0 +1,10 @@
+package com.example.littlelemoncapstone
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun HomeScreen(navController: NavHostController) {
+    Text(text = "Home Screen")
+}
