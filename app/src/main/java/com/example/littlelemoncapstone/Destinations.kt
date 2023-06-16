45dece3c3a23ddc8cbe7b7ce726f1df7074942af
Antoni Luthfi <luthfiantoni027@gmail.com>
2023-06-16 14:30:39 +0800

    create destination routes

---
+++ app/src/main/java/com/example/littlelemoncapstone/Destinations.kt
@@ -0,0 +1,13 @@
+package com.example.littlelemoncapstone
+
+interface Destinations {
+    val route: String
+}
+
+object Home: Destinations {
+    override val route = "Home"
+}
+
+object Onboarding: Destinations {
+    override val route = "Onboarding"
+}
